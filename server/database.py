a2dafcadda06640c20c911aa1acb72475699e716
hmg3845 <hmg3845@rit.edu>
2019-09-21 18:52:30 -0400

    Merge branch 'master' of github.com:jzaia18/SafetyGauntlet

--- server/database.py
+++ server/database.py
@@ -1,5 +1,8 @@
 from configparser import ConfigParser
 import pymodm
+import hashlib
+import binascii
+import os
 
 
 def init(config: ConfigParser):
@@ -57,3 +60,52 @@ class Temperature(pymodm.MongoModel):
     temp_value = pymodm.FloatField()
     user = pymodm.ReferenceField(User)
     timestamp = pymodm.TimestampField()
+
+
+class AdminAccount(pymodm.MongoModel):
+    """
+    Represents the account of an admin that has the ability to make changes to the system
+    including changing user data and lock data
+
+    user_name (CharField): The user name used as the identifier for the admin in the system
+    password (CharField): The password of the admin to login to the system
+    """
+    user_name = pymodm.CharField()
+    password = pymodm.CharField()
+
+
+def _hash_password(password: str) -> str:
+    """
+    Hash the provided password using sha512
+    """
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac(
+        'sha512', password.encode('utf-8'), salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
+
+
+def _verify_password(provided_password: str, correct_password: str) -> bool:
+    """
+    Verify a stored password against one provided by user
+
+    provided_password (str): The plain text password attempting to be used to login
+    correct_password (str): The hashed password that is the correct password
+    """
+    salt = correct_password[:64]
+    correct_password = correct_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac(
+        'sha512', provided_password.encode('utf-8'), salt.encode('ascii'), 100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == correct_password
+
+
+def verify_password(admin: AdminAccount, provided_password: str) -> bool:
+    """
+    Checks to see if the password provided for the given user name is valid
+
+    admin (AdminAccount): The admin that is attempting to login
+    provided_password (str): The plain text password provided by the user
+    """
+    correct_password = admin.password
+    return _verify_password(provided_password, correct_password)
