ba4cc92e0be3be770c9dd6a3a03d0ca93d9f3ba6
Collin Bolles <collinbolles@gmail.com>
2019-09-21 00:10:35 -0400

    Adding ability to check if a user has access to a specific lock

--- server/database.py
+++ server/database.py
@@ -23,7 +23,31 @@ def init(config: ConfigParser):
 
 
 class User(mdb.Document):
+    """
+    Represents a single user that is kept track of by the server. Each other datapoint
+    is associated with a user
+
+    user_id (SequenceField): The unique of the user set automatically
+    rfid (IntField): The id of the rfid chip associated with the user
+    first_name (StringField): The first name of the user
+    last_name (StringField): The last name of the user
+    """
     user_id = mdb.SequenceField(required=True, primary_key=True)
     rfid = mdb.IntField()
     first_name = mdb.StringField(required=True)
     last_name = mdb.StringField(required=True)
+
+
+class Lock(mdb.Document):
+    """
+    Represents a single smart lock that user can potentially access via their RFID chip.
+    Each room keeps track of a list of users that can access the lock as well as other
+    information about the lock.
+
+    lock_id (SequenceField): The unique id of the lock set automatically
+    description (StringField): A human readable description of the lock
+    accepted_users (ListField(ReferenceField('User'))): The users that can unlock the lock
+    """
+    lock_id = mdb.SequenceField(required=True, primary_key=True)
+    description = mdb.StringField()
+    accepted_users = mdb.ListField(mdb.ReferenceField('User'))
